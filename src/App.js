28b86a8688cb173da5bb50b2350caa463541fd5d
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-05-29 18:58:35 +0530

    face detection works at this point

--- src/App.js
+++ src/App.js
@@ -1,20 +1,69 @@
+import React from 'react';
 import './App.css';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
-function App() {
-  return (
-    <div className="App">
+const app = new Clarifai.App({
+  apiKey: '54b3e1431662477fad0841f751f16c02'});
 
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <Face Recognition /> */}
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      input: "",
+      imageUrl: "",
+      box: {},
+    }
+  }
+  onInputChange = (event) =>{
+    this.setState({input: event.target.value});
+  }
+
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    //console.log(clarifaiFace);
+    return({
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+      rightCol: width - (clarifaiFace.right_col * width),
+    })
+  }
+
+  displayFaceBox = (box) =>{
+    this.setState({box: box});
+    console.log(box);
+  }
+
+  onSubmit = () =>{
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(response => {
+        this.displayFaceBox(this.calculateFaceLocation(response));
+        },
+        function(err) {
+          // there was an error
+        }
+    );
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'tachyons';
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () =>{
+const ImageLinkForm = ({onInputChange, onSubmit}) =>{
     return(
         <div className="pv5">
             <p className="f3">
@@ -10,8 +10,8 @@ const ImageLinkForm = () =>{
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input type="text" className="center f4 pa2 w-70 center"/>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                    <input type="text" className="center f4 pa2 w-70 center" onChange={onInputChange} />
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onSubmit}>Detect</button>
                 </div>
             </div>        
         </div>
